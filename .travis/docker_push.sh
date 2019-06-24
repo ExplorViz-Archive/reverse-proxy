f768ee3b03d521287b1af5d6d97ca4c788711641
Alexander Krause <akr@informatik.uni-kiel.de>
2019-06-24 11:56:29 +0200

    testing manual travis-ci build for reverse-proxy instead of automatic dockerhub build

---
+++ .travis/docker_push.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Building Docker image"
+
+echo $DOCKER_PW | docker login -u $DOCKER_LOGIN --password-stdin
+
+if [[ "$TRAVIS_BRANCH" == 'dev-1' ]]; then
+
+  docker build -t reverse-proxy .
+  docker tag reverse-proxy explorviz/reverse-proxy:dev-$TRAVIS_COMMIT
+  docker tag reverse-proxy explorviz/reverse-proxy:dev
+
+  docker build -t reverse-proxy -f Dockerfile_macos .
+  docker tag reverse-proxy explorviz/reverse-proxy:macos-dev-$TRAVIS_COMMIT
+  docker tag reverse-proxy explorviz/reverse-proxy:macos-dev
+
+  docker push explorviz/reverse-proxy
+
+elif [[ "$TRAVIS_BRANCH" == 'master' ]]; then
+
+  docker build -t reverse-proxy .
+  docker tag reverse-proxy explorviz/reverse-proxy:dev-$TRAVIS_COMMIT
+  docker tag reverse-proxy explorviz/reverse-proxy:dev
+
+  docker build -t reverse-proxy -f Dockerfile_macos .
+  docker tag reverse-proxy explorviz/reverse-proxy:macos-dev-$TRAVIS_COMMIT
+  docker tag reverse-proxy explorviz/reverse-proxy:macos-dev
+  
+  docker push explorviz/reverse-proxy
+
+else
+  echo "Unknown branch for Docker image."
+fi
